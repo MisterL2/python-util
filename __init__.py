ee1c7ee3424c0f1d7bc2b8529168c2fcbe920ad8
MisterL2 <46602881+MisterL2@users.noreply.github.com>
2019-05-09 17:39:26 +0200

    ready for pypi

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#(c) MisterL2
-
-from .haskell import *
-from .general import *
-from .math import *
-from .files import *
-from .data_structures import *
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py3utils",
+    version="1.0",
+    author="MisterL2",
+    author_email="misterl2@gmx.net",
+    description="A small python utilities addon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MisterL2/python-util",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
