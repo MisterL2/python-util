56f16ffbebca7c22dc7cdd1b7a1ad068f2a92f69
MisterL2 <46602881+MisterL2@users.noreply.github.com>
2019-05-09 18:00:57 +0200

    Added gitignore and added to PyPi package index
    HYPE!
    How on earth was that name not taken

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py3utils",
+    name="python-util",
     version="1.0",
     author="MisterL2",
     author_email="misterl2@gmx.net",
